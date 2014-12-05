c3a65f6af9f2cbffa1a1ce39c216d32fd0fe025a
Chandima Rajaguru <chandima.rajaguru@attuneconsulting.com>
2014-12-05 11:40:55 +0530

    Adding jars and class files

---
+++ src/com/attune/main/ExcelDataReader.java
@@ -0,0 +1,34 @@
+package com.attune.main;
+
+import com.attune.model.Product;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chandima.Rajaguru on 12/5/2014.
+ */
+public class ExcelDataReader {
+
+    public List<Product> readData(){
+        List<Product> productList = new ArrayList<Product>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream("resources/input/Products_Extract.xlsx");
+            XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+            Sheet worksheet = workbook.getSheet("Sheet1");
+            
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ src/com/attune/model/Product.java
@@ -0,0 +1,45 @@
+package com.attune.model;
+
+/**
+ * Created by Chandima.Rajaguru on 12/5/2014.
+ */
+public class Product {
+
+    private String style;
+    private String color;
+    private String size;
+    private String upc;
+
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getUpc() {
+        return upc;
+    }
+
+    public void setUpc(String upc) {
+        this.upc = upc;
+    }
+}

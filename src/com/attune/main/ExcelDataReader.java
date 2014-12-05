ab4d043cf83bb2d4e2209ba47e5e646266d2f7af
Chandima Rajaguru <chandima.rajaguru@attuneconsulting.com>
2014-12-05 11:59:12 +0530

    Reading Excel fiile

--- src/com/attune/main/ExcelDataReader.java
+++ src/com/attune/main/ExcelDataReader.java
@@ -1,6 +1,8 @@
 package com.attune.main;
 
 import com.attune.model.Product;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -8,6 +10,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -22,13 +25,41 @@ public class ExcelDataReader {
             XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
             Sheet worksheet = workbook.getSheet("Sheet1");
 
+            Iterator<Row> rowIterator = worksheet.iterator();
+            Long count = 0L;
+            while(rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+
+                //For each row, iterate through each columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while(cellIterator.hasNext()) {
+
+                    Cell cell = cellIterator.next();
+
+                    switch(cell.getCellType()) {
+                        case Cell.CELL_TYPE_BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                    }
+                }
+                System.out.println("");
+            }
+            fileInputStream.close();
 
         }catch (FileNotFoundException e) {
             e.printStackTrace();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+
+        return productList;
     }
 
 }

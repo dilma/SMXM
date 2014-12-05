aa2a9c3b67c0e68dc2037757776fef6be375c65b
Madhawa Chandrasena <madhawa.chandrasena@attuneconsulting.com>
2014-12-05 13:27:55 +0530

    Microsoft JDBC driver added

--- src/com/attune/main/dbread/DBConnection.java
+++ src/com/attune/main/dbread/DBConnection.java
@@ -9,36 +9,20 @@ public class DBConnection {
 
     static final String userName = "SM-LocalSiteDBUser";
     static final String password = "st3v3m@dden!";
-    static final String url = "jdbc:microsoft:sqlserver://localhost:1433" + ";databaseName=sm_content_local";
+    static final String url = "jdbc:sqlserver://localhost:1433" + ";databaseName=sm_content_local";
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             Connection con = DriverManager.getConnection(url, userName, password);
-            PreparedStatement ps = con.prepareStatement("SELECT  V.SKU" +
-                    " FROM [sm_content_local].[dbo].[PRODUCT] PRO WITH (NOLOCK)" +
-                    " LEFT JOIN [sm_content_local].[dbo].[PRODUCT_VARIANT] V  WITH (NOLOCK)" +
-                    " ON PRO.PRODUCT_ID =  V.PRODUCT_ID" +
-                    " WHERE V.SKU IS NOT NULL AND LEN(V.SKU) > 10" +
-                    " AND PRO.STYLE = ? AND V.SIZE_NAME = ? AND V.COLOR_NAME=?");
-/*
-            String[] result = new String[20];
-            if (rs != null) {
-                while (rs.next()) {
-                    for (int i = 0; i < result.length; i++) {
-                        for (int j = 0; j < result.length; j++) {
-                            result[j] = rs.getString(i);
-                            System.out.println(result[j]);
-                        }
-                    }
-                }
-            }*/
+            return con;
 
-        } catch (ClassNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
 
-        return null;
+
     }
 
 
--- src/com/attune/main/dbread/DBReader.java
+++ src/com/attune/main/dbread/DBReader.java
@@ -1,7 +1,52 @@
 package com.attune.main.dbread;
 
+import com.attune.model.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Madhawa.Chandrasena on 12/5/2014.
  */
 public class DBReader {
+    public static void main(String[] args) {
+        DBReader db = new DBReader();
+        Product p = new Product();
+        p.setColor("BLACK LEATHER");
+        p.setSize("10.5");
+        p.setStyle("SERPENT");
+        List<Product> list = new ArrayList<Product>();
+        list.add(p);
+        db.excuteQuery(list);
+    }
+    public void excuteQuery(List<Product> products) {
+
+        try {
+            Connection con = DBConnection.getConnection();
+            for (Product p : products) {
+                PreparedStatement ps = con.prepareStatement("SELECT  V.SKU FROM [sm_content_local].[dbo].[PRODUCT] PRO WITH (NOLOCK) LEFT JOIN [sm_content_local].[dbo].[PRODUCT_VARIANT] V  WITH (NOLOCK) ON PRO.PRODUCT_ID =  V.PRODUCT_ID WHERE V.SKU IS NOT NULL AND LEN(V.SKU) > 10 AND PRO.STYLE = ? AND V.SIZE_NAME = ? AND V.COLOR_NAME=?");
+                ps.setString(1, p.getStyle());
+                ps.setString(2, p.getSize());
+                ps.setString(3, p.getColor());
+
+                ResultSet rs = ps.executeQuery();
+
+                if (rs != null) {
+                    while (rs.next()) {
+                       String s = rs.getString(1);
+                        System.out.println("sa----"+s);
+                    }
+                }
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
